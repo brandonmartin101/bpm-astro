cfac0bcd9fdb4444064642e99855150d3af48d35
bmartin-dealersocket <bmartin@dealersocket.com>
2023-10-09 10:28:44 -0500

    Buncha changes

---
+++ src/components/react/MuiReference.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react'
+import Button from '@mui/material/Button'
+
+// React function to show all Mui components in a single place
+export const MuiReference = () => {
+  return (
+    <Button variant='contained' color='primary'>
+      Button reference
+    </Button>
+  )
+}
---
+++ src/components/react/ToDoList.tsx
@@ -0,0 +1,5 @@
+// React function
+
+export const ToDoList = () => {
+  return <div>ToDoList</div>
+}
