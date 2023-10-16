97221df5c9f6d87be84bf12f97e20d89eaf9d6d6
bmartin-dealersocket <bmartin@dealersocket.com>
2023-10-16 16:35:10 -0500

    misc updates

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,18 @@
-import { defineConfig } from 'astro/config';
-import react from '@astrojs/react';
-import svelte from '@astrojs/svelte';
-import vue from '@astrojs/vue';
-import solid from '@astrojs/solid-js';
-import sitemap from '@astrojs/sitemap';
-import netlify from "@astrojs/netlify/functions";
+import { defineConfig } from 'astro/config'
+import react from '@astrojs/react'
+import svelte from '@astrojs/svelte'
+import vue from '@astrojs/vue'
+import solid from '@astrojs/solid-js'
+import sitemap from '@astrojs/sitemap'
+import netlify from '@astrojs/netlify/functions'
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://brandonmartin.dev',
   integrations: [react(), svelte(), vue(), solid(), sitemap()],
-  output: "server",
-  adapter: netlify()
-});
+  output: 'server',
+  adapter: netlify(),
+  experimental: {
+    viewTransitions: true,
+  },
+})
