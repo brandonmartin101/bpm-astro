7f660bf1c479b8d4e74919bf751137b65dca5c3a
bmartin-dealersocket <bmartin@dealersocket.com>
2023-10-18 23:30:01 -0500

    typer ain updates

--- src/pages/projects/typer-ain/typer-ain.js
+++ src/pages/projects/typer-ain/typer-ain.js
@@ -1,24 +1,29 @@
 let ain = {
+  // DOM sections
   gameArea: document.querySelector('#ain-game'),
   scoreArea: {
     currentScore: document.querySelector('#ain-current-score'),
     highScore: document.querySelector('#ain-high-score'),
-    speed: document.querySelector('#ain-speed'),
   },
-  currentScore: 0,
-  highScore: 0,
-  speed: 1,
-  chars: [], // [{ letter:'a', x:0, y:0 },...]
-  getRandomChar: () => {
-    //TODO add uppercase or special characters
-    // const upperCase = Math.random() < 0.5
-    // const charCode = upperCase
-    //   ? Math.floor(Math.random() * 26) + 65
-    //   : Math.floor(Math.random() * 26) + 97
 
-    //* caseless
-    const charCode = Math.floor(Math.random() * 26) + 97
-    return String.fromCharCode(charCode)
+  // game variables
+  currentScore: 0,
+  highScore: localStorage.getItem('highScore') || 0,
+  chars: [], // [{ letter:'a', x:0, y:0 },...]
+  deadChars: [],
+
+  // game functions
+  getRandomChar: (special) => {
+    if (special) {
+      //* special character
+      return String.fromCharCode(Math.floor(Math.random() * 33) + 32)
+    } else {
+      //* caseless
+      return String.fromCharCode(Math.floor(Math.random() * 26) + 97)
+    }
+  },
+  getRandomX: () => {
+    return Math.floor(Math.random() * (ain.gameArea.width - 20) + 10)
   },
 }
 
@@ -26,14 +31,14 @@ let ain = {
 const ainLoop = () => {
   const ctx = ain.gameArea.getContext('2d')
   ctx.clearRect(0, 0, ain.gameArea.width, ain.gameArea.height)
-
   ctx.font = '18px monospace'
 
   // generate char
+  ain.charSpawnRate = 0.03 + ain.currentScore * 0.0005
   if (Math.random() < ain.charSpawnRate) {
     const newChar = {
       letter: ain.getRandomChar(),
-      x: Math.round(Math.random() * ain.gameArea.width),
+      x: ain.getRandomX(),
       y: 0,
     }
     ain.chars.push(newChar)
@@ -42,18 +47,40 @@ const ainLoop = () => {
   ain.chars.forEach((char, index) => {
     // move chars
     //TODO make the speed variable for each char
-    char.y += 1 * ain.speed
+    char.y += 1
+    if (char.y > ain.gameArea.height) {
+      // letter was missed
+      ain.currentScore -= 5
+      ain.deadChars.push(char)
+      ain.chars.splice(index, 1)
+    }
     // place chars on screen
+    ctx.fillStyle = 'black'
     ctx.fillText(char.letter, char.x, char.y)
   })
 
-  if (ain.chars.length > 20) {
+  ain.deadChars.forEach((char, index) => {
+    char.y -= 1
+    if (char.y < 0) {
+      ain.deadChars.splice(index, 1)
+    }
+    ctx.fillStyle = 'red'
+    ctx.fillText(char.letter, char.x, char.y)
+  })
+
+  if (ain.chars.length > 100) {
     ain.chars.shift()
   }
 
+  // handle score output
+  if (ain.currentScore < 0) ain.currentScore = 0
+  if (ain.currentScore > ain.highScore) {
+    ain.highScore = ain.currentScore
+    localStorage.setItem('ainHighScore', ain.highScore)
+  }
   ain.scoreArea.currentScore.textContent = `CurrentScore: ${ain.currentScore}`
   ain.scoreArea.highScore.textContent = `High Score: ${ain.highScore}`
-  ain.scoreArea.speed.textContent = `Speed: ${ain.speed}`
+
   requestAnimationFrame(ainLoop)
 }
 
@@ -69,14 +96,17 @@ document.addEventListener('keypress', (e) => {
     }
   }
   if (!matched) {
-    console.log('bad key press')
     ain.currentScore--
+    ain.deadChars.push({
+      letter: ain.getRandomChar(true),
+      x: ain.getRandomX(),
+      y: ain.gameArea.height,
+    })
   }
 })
 document.querySelector('#ain-start').addEventListener('click', (e) => {
   e.target.toggleAttribute('disabled')
   ain.currentScore = 0
   ain.charSpawnRate = 0.03
-  ain.speed = 1
   ainLoop()
 })
